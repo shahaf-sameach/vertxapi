9517881c9f479f4f24e671b2309eb90531779192
shahaf <shahaf@minute.ly>
2017-08-25 16:22:31 +0300

    removed current time print for debug

--- src/main/java/com/example/app/Main.java
+++ src/main/java/com/example/app/Main.java
@@ -10,7 +10,6 @@ public class Main {
     public static void main(String[] args) {
         ProductRepo.seed();
         CampaignRepo.seed();
-        System.out.println(System.currentTimeMillis());
 
         Vertx.vertx().deployVerticle(new SimpleAPI(), res -> {
             if (res.succeeded()) {
